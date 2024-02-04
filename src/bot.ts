b5fee224f7b14839b338b2415c0b57d50268fc5c
Julius Gmeinder <julius.gmeinder@gmail.com>
2024-02-04 23:19:41 +0100

    todos

--- src/bot.ts
+++ src/bot.ts
@@ -9,26 +9,38 @@ const client = new Client({
 
 client.once("ready", () => {
 
+    // Refresh commands in 2 test guild
+    //
     // (async () => {
     //   await deployCommands({guildId: config.DISCORD_DEV_GUILD_ID_1});
     //   await deployCommands({guildId: config.DISCORD_DEV_GUILD_ID_2});
     // })()
 
+    // TODO: Call API for every server the client is on, to add every guild to the db which invited the bot when it was offline
+    //
+    // ...
+    //
+
     console.log("Discord bot is ready! 🤖");
 });
 
 client.on("guildCreate", async (guild) => {
-    await deployCommands({ guildId: guild.id });
+  await deployCommands({ guildId: guild.id });
+  // TODO: add Guild to DB
+});
+
+client.on("guildDelete", function(guild){
+  // TODO: delete Guild from DB
 });
 
 client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-      return;
-    }
-    const { commandName } = interaction;
-    if (commands[commandName as keyof typeof commands]) {
-      commands[commandName as keyof typeof commands].execute(interaction);
-    }
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    commands[commandName as keyof typeof commands].execute(interaction);
+  }
 });
 
 client.login(config.DISCORD_TOKEN);
